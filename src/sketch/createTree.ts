14f77f4b7c6f48273695ccdf3fb41096007cae0e
Emanuel Sharma <send.emanuel@hotmail.com>
2025-02-24 12:13:03 -0500

    Use textures to allow bigger trees

--- src/sketch/createTree.ts
+++ src/sketch/createTree.ts
@@ -1,83 +1,103 @@
 import * as THREE from 'three';
 
+const vertexShader = `
+  precision mediump float;
+
+  uniform mat4 u_modelViewMatrix;
+  uniform mat4 u_projectionMatrix;
+  uniform sampler2D u_nodeData;
+  uniform float u_branchAngle;
+  uniform float u_nodeDataSize;
+  uniform float u_age;
+
+  out float f;
+
+  vec3 addLevel(float length, float angleA, float angleB, vec3 crrtPos, vec3 lastPos) {
+    vec3 direction = normalize(crrtPos - lastPos);
+    vec3 orthoA = normalize(cross(direction, vec3(0.0001,1.,0.)));
+    vec3 orthoB = normalize(cross(vec3(0.0001,1.,0.), orthoA));
+    return crrtPos + length * (sin(angleB) * (cos(angleA) * orthoA + sin(angleA) * orthoB) + cos(angleB) * direction);
+  }   
+
+  void main() {
+    float index = float(gl_InstanceID + 1);
+    float nodeDepth = floor(log2(index));
+
+    vec3 lastPos = vec3(0.1, -1., 0.);
+    vec3 crrtPos = vec3(0., 0., 0.);
+    float depth = 0.;
+    float branchMaturity = clamp(0., u_age - nodeDepth, 1.);
+    while (depth <= nodeDepth) {
+      float branchMaturity2 = clamp(0., .5 * (u_age - depth), 1.);
+      float crrtIndex = floor(index / pow(2., nodeDepth - depth)) - 1.;
+      float isLeaf = step(-0.5, depth - nodeDepth);
+      vec2 nodeData = texture(u_nodeData, vec2((crrtIndex + 0.5) / u_nodeDataSize, 0.5)).xy;
+      vec3 tempPos = addLevel(nodeData.x, 6.19 * nodeData.y, branchMaturity2 * u_branchAngle * mod(crrtIndex, 2.), crrtPos, lastPos);
+      lastPos = crrtPos;
+      crrtPos = tempPos;
+      depth = depth + 1.;
+    }
+
+    float vertex = branchMaturity * float(gl_VertexID);
+    vec3 localPos = mix(lastPos, crrtPos, vertex);
+    vec4 viewSpacePos = u_projectionMatrix * u_modelViewMatrix * vec4(localPos, 1.);
+    gl_Position = viewSpacePos;
+    f = nodeDepth + vertex;
+  }
+  `
+
+const fragmentShader = `
+  precision mediump float;
+  
+  uniform sampler2D u_nodeData;
+  uniform float u_treeDepth;
+
+  in float f;
+
+  out vec4 color;
+
+  void main() {
+    color = vec4(0.6, 0.5 +  0.3 * f / u_treeDepth, 0.4, 1.);
+  }
+  `
+
 function gaussianRandom(mean = 0, stdev = 1) {
-  const u = 1 - Math.random(); // Converting [0,1) to (0,1]
+  const u = 1 - Math.random();
   const v = Math.random();
   const z = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
-  // Transform to the desired mean and standard deviation:
   return z * stdev + mean;
 }
 
 export function createTree() {
-  const numNodes = 1000;
-  const maxDepth = Math.ceil(Math.log2(numNodes + 1));
-  const lengths = Array.from({ length: numNodes }, (_, i) => 0.3 * gaussianRandom(1 + (maxDepth - Math.ceil(Math.log2(i + 1)))));
-  const angles = Array.from({ length: numNodes }, () => 2 * 3.145 * Math.random());
-  const index = Array.from({ length: numNodes }, (_, i) => i + 1);
+  const depth = 14;
+  const numNodes = 2 ** depth - 1;
+  const nodeData = Array.from({ length: numNodes + 1 }, (_, i) => {
+    const level = Math.floor(Math.log2(i + 1));
+    const length = 255 * Math.max(0, Math.min(1, gaussianRandom(2 + depth - level) / (depth + 2)));
+    const angle = 255 * Math.random();
+    return [length, angle];
+  }).flat();
+  const nodeDataTexture = new THREE.DataTexture(new Uint8Array(nodeData), numNodes + 1, 1, THREE.RGFormat, THREE.UnsignedByteType);
+  nodeDataTexture.needsUpdate = true;
 
-  console.log(lengths);
-
-  const vertexShader = `
-    precision mediump float;
-
-    uniform float u_length[${numNodes}]; 
-    uniform float u_angle[${numNodes}];
-    attribute float index;
-
-    varying float v_depth;
-
-    void main() {
-      vec3 lastPos = vec3(0., 0., 0.);
-      vec3 crrtPos = vec3(0.01, 1., 0.);
-      float nodeDepth = floor(log2(index));
-      float depth = 0.;
-      // We need to iterate over each level from root to tip
-      while (depth < nodeDepth + 0.5) {
-        float crrtIndex = floor(index / pow(2., nodeDepth - depth)) - 1.;
-        float parity = mod(crrtIndex, 2.);
-
-        float length = u_length[int(crrtIndex)];
-        float angleA = u_angle[int(crrtIndex)];
-        float angleB = parity * 3.145 / 6.;
-        vec3 direction = normalize(crrtPos - lastPos);
-        vec3 orthoA = normalize(cross(direction, vec3(0.,1.,0.)));
-        vec3 orthoB = normalize(cross(vec3(0.,1.,0.), orthoA));
-        lastPos = crrtPos;
-        //crrtPos = crrtPos + length * vec3(sin(angleB), cos(angleB), 0.);
-        crrtPos = crrtPos + length * (sin(angleB) * (cos(angleA) * orthoA + sin(angleA) * orthoB) + cos(angleB) * direction);
-        
-        depth = depth + 1.;
-      }
-      gl_Position = projectionMatrix * modelViewMatrix * vec4(mix(lastPos, crrtPos, position.y), 1.);
-      v_depth = floor(log2(index));
-    }
-    `
-
-  const fragmentShader = `
-    precision mediump float;
-
-    varying float v_depth;
-
-    void main() {
-      vec3 colorA = vec3(0.,0.,1.);
-      vec3 colorB = vec3(0.75, 0.75, 1.);
-      gl_FragColor = vec4(mix(colorA, colorB, v_depth / ${maxDepth}.), 1.);
-    }
-    `
-
-  const material = new THREE.ShaderMaterial({
+  const material = new THREE.RawShaderMaterial({
     uniforms: {
-      'u_length': { value: lengths },
-      'u_angle': { value: angles },
+      'u_nodeData': { value: nodeDataTexture },
+      'u_treeDepth': { value: depth },
+      'u_branchAngle': { value: Math.PI / 6 },
+      'u_nodeDataSize': { value: numNodes + 1 },
+      'u_age': { value: 100 },
+      'u_modelViewMatrix': { value: new THREE.Matrix4() },
+      'u_projectionMatrix': { value: new THREE.Matrix4() },
     },
     vertexShader: vertexShader,
     fragmentShader: fragmentShader,
+    glslVersion: THREE.GLSL3,
   });
   const geometry = new THREE.InstancedBufferGeometry();
-  geometry.setAttribute('index', new THREE.InstancedBufferAttribute(new Float32Array(index), 1, undefined, 1));
-  geometry.setAttribute('position', new THREE.BufferAttribute(new Float32Array([0, 0, 0, 0, 1, 0]), 3));
-  geometry.instanceCount = index.length;
-  geometry.boundingSphere = new THREE.Sphere(new THREE.Vector3(), 4.);
+  geometry.setAttribute('position', new THREE.BufferAttribute(new Float32Array([0, 1]), 1));
+  geometry.boundingSphere = new THREE.Sphere(undefined, 10);
+  geometry.instanceCount = numNodes;
   const line = new THREE.LineSegments(geometry, material);
 
   return line;
--- src/sketch/sketchWindow.tsx
+++ src/sketch/sketchWindow.tsx
@@ -9,48 +9,55 @@ interface SketchWindowProps {
 const SketchWindow: React.FC<SketchWindowProps> = (props: SketchWindowProps) => {
   const canvasRef = React.useRef<HTMLCanvasElement>(null);
   React.useEffect(() => {
-      const canvas = canvasRef.current;
-      if (!canvas) {
-        return;
-      }
-      const scene = new THREE.Scene();
-      const tree = createTree();
-      scene.add( tree );
+    const canvas = canvasRef.current;
+    if (!canvas) {
+      return;
+    }
+    const scene = new THREE.Scene();
+    const tree = createTree();
+    scene.add(tree);
 
-      const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-      const controls = new OrbitControls( camera, canvas );
-      camera.position.set( 0, 20, 20 );
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const controls = new OrbitControls(camera, canvas);
+    camera.position.set(20, 5, 20);
+    controls.update();
+
+    const startTime = (new Date()).getTime() / 1000;
+    function animate() {
+      resizeCanvasToDisplaySize();
       controls.update();
+      const time = (new Date()).getTime() / 1000;
+      tree.material.uniforms['u_branchAngle'].value = Math.PI / 6 + 0.03 * Math.sin(time);
+      tree.material.uniforms['u_age'].value = 4 * (time - startTime);
+      const modelViewMatrix = tree.modelViewMatrix.multiplyMatrices(camera.matrixWorldInverse, tree.matrixWorld);
+      tree.material.uniforms['u_modelViewMatrix'].value = modelViewMatrix;
+      tree.material.uniforms['u_projectionMatrix'].value = camera.projectionMatrix;
+      renderer.render(scene, camera);
+    }
 
-      function animate() {
-        resizeCanvasToDisplaySize();
-        controls.update();
-        renderer.render( scene, camera );
-      }
+    const renderer = new THREE.WebGLRenderer({
+      antialias: true,
+      canvas
+    });
+    renderer.setSize(canvas.width, canvas.height, false)
+    renderer.setClearColor(0x262244, 1);
+    renderer.render(scene, camera);
+    renderer.setAnimationLoop(animate);
 
-      const renderer = new THREE.WebGLRenderer({
-        antialias:true,
-        canvas
-      });
-      renderer.setSize(canvas.width , canvas.height , false)
-      renderer.setClearColor( 0x000000, 1 );
-      renderer.render( scene, camera );
-      renderer.setAnimationLoop( animate );
-
-      function resizeCanvasToDisplaySize() {
-        const canvas = renderer.domElement;
-        const width = canvas.clientWidth;
-        const height = canvas.clientHeight;
-        renderer.setSize(width, height, false);
-        camera.aspect = width / height;
-        camera.updateProjectionMatrix();
-      }
-    }, 
+    function resizeCanvasToDisplaySize() {
+      const canvas = renderer.domElement;
+      const width = canvas.clientWidth;
+      const height = canvas.clientHeight;
+      renderer.setSize(width, height, false);
+      camera.aspect = width / height;
+      camera.updateProjectionMatrix();
+    }
+  },
     [canvasRef]
-  )
+  );
 
   return (
-    <canvas style={{width: '100%', height: '100%', position: 'absolute'}} ref={canvasRef} />
+    <canvas style={{ width: '100%', height: '100%', position: 'absolute' }} ref={canvasRef} />
   );
 };
 
